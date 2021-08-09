d88bc9bcda54c8be2479021d89d03946f104c98c
zainabsaeed <zainabsaeed786@gmail.com>
2021-08-09 07:48:55 +0500

    complete website layout

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
+//services
+var datascienceRouter = require('./routes/services/data-science');
+
+//case studies
+var casestudy1Router = require('./routes/casestudies/casestudy-1');
+
+//demos
+var demosRouter = require('./routes/demos/demos');
+
+//innovation & labs
+var inoovationlabsRouter = require('./routes/innovations-labs/innovations-labs');
+
+//clients
+var clientsRouter  = require('./routes/clients/clients');
+
+//team
+var teamRouter = require('./routes/team/team');
+
+//blog
+var blogRouter = require('./routes/blog');
+var blogsampleRouter = require('./routes/blogs/blog-sample');
+
+//contact
+var contactRouter  =require('./routes/contact/contact');
+
 var app = express();
 
 // view engine setup
@@ -19,6 +44,31 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+//services
+app.use('/data-science',datascienceRouter);
+
+//case studies
+app.use('/casestudy-1',casestudy1Router);
+
+//demos
+app.use('/demos',demosRouter);
+
+//innovations & labs
+app.use('/innovations-labs',inoovationlabsRouter);
+
+//clients
+app.use('/clients',clientsRouter);
+
+//team
+app.use('/team',teamRouter);
+
+//blog
+app.use('/blog', blogRouter);
+app.use('/blogs/blog-sample',blogsampleRouter);
+
+//contact
+app.use('/contact',contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/blogs/blog-sample.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('blogs/blog-sample', { title: 'Future.ai|An AI and Data Science Company' });
+});
+
+
+
+
+
+
+
+module.exports = router;
